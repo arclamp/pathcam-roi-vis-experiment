33f2cd83043cf26c8883fae1952b6e524ed6b608
Roni Choudhury <roni.choudhury@kitware.com>
2024-02-08 14:24:05 -0500

    Refactor demo drawing code into a function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-console.log('hi');
-
 const canvas = document.getElementById('canvas');
 if (!canvas.getContext) {
   throw new Error('canvas not supported in this browser.');
 }
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = 'rgb(200 0 0)';
-ctx.fillRect(10, 10, 50, 50);
+function draw() {
+  ctx.fillStyle = 'rgb(200 0 0)';
+  ctx.fillRect(10, 10, 50, 50);
 
-ctx.fillStyle = 'rgb(0 0 200 / 50%)';
-ctx.fillRect(30, 30, 50, 50);
+  ctx.fillStyle = 'rgb(0 0 200 / 50%)';
+  ctx.fillRect(30, 30, 50, 50);
+}
+
+draw();
